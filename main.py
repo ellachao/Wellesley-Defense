ec03c5aba17cc741774baf5105952eeda4dac859
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-04-18 10:32:46 -0400

    health bar and attack system working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame 
 from models import enemy_test
+from math import sqrt, pow
 
 class View:
     """
@@ -16,9 +17,13 @@ class View:
     def update(self):
         pic = pygame.image.load("map.png").convert()
         self.screen.blit(pic, (0, 0))
-        pygame.draw.circle(self.screen, self.model.enemy_test.color, 
-            self.model.enemy_test.pos, self.model.enemy_test.radius, 
-            self.model.enemy_test.width)
+
+        if (not self.model.enemy_test.death):
+            pygame.draw.circle(self.screen, self.model.enemy_test.color, 
+                self.model.enemy_test.pos, self.model.enemy_test.radius, 
+                self.model.enemy_test.width)
+            pygame.draw.line(self.screen, (255, 0, 0), [self.model.enemy_test.pos[0] - 10, self.model.enemy_test.pos[1] - 15], 
+                [self.model.enemy_test.pos[0] - 10 + self.model.enemy_test.health / 3, self.model.enemy_test.pos[1] - 15], 4)
         pygame.display.update()
 
 class Model:
@@ -33,12 +38,14 @@ class Model:
         'green': (0, 255, 0),
         'blue': (0, 0, 255)
         }
-        self.enemy_test = enemy_test(color_list['blue'], [10, 240], [10, 240], 10, 0, False, 5, 50)
+        self.enemy_test = enemy_test(color_list['blue'], [10, 240], [10, 240], 10, 0, False, 10, 50, False)
 
     def update(self):
         if (self.enemy_test.moving == True):
             self.enemy_test.pos[0] = self.enemy_test.speed * (pygame.time.get_ticks() / 1000) + self.enemy_test.start_pos[0]
 
+        if (self.enemy_test.health <= 0):
+            self.enemy_test.death = True
 
 
 class Controller:
@@ -47,9 +54,24 @@ class Controller:
     """
     def __init__(self, model):
         self.model = model
+        self.last_click_time = 0
 
     def start(self):
         self.model.enemy_test.moving = True
+
+    def update(self):
+        if (pygame.mouse.get_pressed() == (1, 0, 0) and pygame.time.get_ticks() - self.last_click_time > 100):
+            self.last_click_time = pygame.time.get_ticks()
+            p = pygame.mouse.get_pos(0)
+            x = p[0] 
+            y = p[1]
+            flag = True
+            if (sqrt(pow((x - self.model.enemy_test.pos[0]), 2) + pow((y - self.model.enemy_test.pos[1]), 2)) 
+                <= self.model.enemy_test.radius
+                and flag):
+                self.model.enemy_test.health -= 10
+                print self.model.enemy_test.health
+                flag = False
 
 def main():
     # initialize the pygame environment
@@ -77,6 +99,7 @@ def main():
         view.update()
         controller.start()
         model.update()
+        controller.update()
 
 if __name__ == "__main__":
     main()
