e222fe154b3e1baebde1c9b2c6a5c4d3bd246000
ellachao <hchao@wellesley.edu>
2015-04-18 12:49:19 -0400

    more map things

--- map.py
+++ map.py
@@ -11,5 +11,23 @@ class Map:
         lines=charmap.readlines()
         for line in lines:
             self.mapArray.append(list(line.strip()))
-
+    
+    def start(self):
+        for i in self.mapArray:
+            if i==0 or i==len(self.mapArray)-1:
+                for j in i:
+                    if j=='o':
+                        return (i,j)
+            else:
+                if i[0]=='o' or i[len(j)-1]=='o':
+                    return (i,j)
+                    
+    #def check(self,preX,preY,curX,curY):
+    #    locL=[(curX-1,curY-1),(curX,curY-1),(curX+1,curY-1),(curX-1,curY),(curX+1,curY),
+    #    (curX-1,curY+1),(curX,curY+1),(curX+1,curY+1)]
+    #    for i in locL:
+    #        if i[0]>=0 and i[0]<=len(self.mapArray)-1 and i[1]
+    #            if i[0]!=preX and i[1]!=preY:
+    #            
         
+            
